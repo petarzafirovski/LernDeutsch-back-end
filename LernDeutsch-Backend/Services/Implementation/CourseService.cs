a5097c7d419f5b32ba79f85c7c46b3f4b3830a4d
Filip Jovanov <filip.jovanov@students.finki.ukim.mk>
2023-04-23 05:44:31 +0200

    Add non-existent services

---
+++ LernDeutsch-Backend/Services/Implementation/CourseService.cs
@@ -0,0 +1,33 @@
+﻿using LernDeutsch_Backend.Models;
+using LernDeutsch_Backend.Repositories;
+
+namespace LernDeutsch_Backend.Services.Implementation
+{
+    public class CourseService : ICourseService
+    {
+        private readonly ICourseRepository _courseRepository;
+
+        public CourseService(ICourseRepository courseRepository)
+        {
+            _courseRepository = courseRepository;
+        }
+
+        public Course Create(Course entity) =>
+            _courseRepository.Create(entity);
+
+        public Course Update(Guid id, Course entity)
+        {
+            // TODO: Finish implementing after mappers
+            throw new NotImplementedException();
+        }
+
+        public Course Delete(Guid id) =>
+            _courseRepository.Delete(id);
+
+        public Course? GetById(Guid id) =>
+            _courseRepository.GetById(id);
+
+        public List<Course> GetAll() =>
+            _courseRepository.GetAll();
+    }
+}
---
+++ LernDeutsch-Backend/Services/Implementation/LessonService.cs
@@ -0,0 +1,33 @@
+﻿using LernDeutsch_Backend.Models;
+using LernDeutsch_Backend.Repositories;
+
+namespace LernDeutsch_Backend.Services.Implementation
+{
+    public class LessonService : ILessonService
+    {
+        private readonly ILessonRepository _lessonRepository;
+
+        public LessonService(ILessonRepository lessonRepository)
+        {
+            _lessonRepository = lessonRepository;
+        }
+
+        public Lesson Create(Lesson entity) =>
+            _lessonRepository.Create(entity);
+
+        public Lesson Update(Guid id, Lesson entity)
+        {
+            // TODO: Finish implementing after mappers
+            throw new NotImplementedException();
+        }
+
+        public Lesson Delete(Guid id) =>
+            _lessonRepository.Delete(id);
+
+        public Lesson? GetById(Guid id) =>
+            _lessonRepository.GetById(id);
+
+        public List<Lesson> GetAll() =>
+            _lessonRepository.GetAll();
+    }
+}
