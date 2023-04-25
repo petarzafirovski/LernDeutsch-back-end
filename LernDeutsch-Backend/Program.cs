f97225d2e226c2debcca584ef21ca8f851957ef5
Petar <zafirovski.petar@gmail.com>
2023-04-25 20:25:50 +0200

    removed services from program.cs

--- LernDeutsch-Backend/Program.cs
+++ LernDeutsch-Backend/Program.cs
@@ -1,14 +1,6 @@
 using LernDeutsch_Backend.Data;
-using LernDeutsch_Backend.Models.Identity;
-using LernDeutsch_Backend.Repositories.Identity;
-using LernDeutsch_Backend.Services.Identity;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
-using Microsoft.OpenApi.Models;
 using System.Security.Cryptography;
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
@@ -18,61 +10,11 @@ configuration["JWT:Secret"] = Convert.ToBase64String(RandomNumberGenerator.GetBy
 
 builder.Services.AddDbContext<ApplicationDatabaseContext>(x => x.UseSqlServer(connectionString));
 
-builder.Services.AddIdentity<BaseUser, IdentityRole>()
-    .AddEntityFrameworkStores<ApplicationDatabaseContext>()
-    .AddDefaultTokenProviders();
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-}).AddJwtBearer(options =>
-{
-    options.SaveToken = true;
-    options.RequireHttpsMetadata = false;
-    options.TokenValidationParameters = new TokenValidationParameters()
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidAudience = configuration["JWT:Audience"],
-        ValidIssuer = configuration["JWT:Issuer"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JWT:Secret"]))
-    };
-});
-
 builder.Services.AddAuthorization();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(opt =>
-{
-    opt.SwaggerDoc("v1", new OpenApiInfo { Title = "LernDeutscheApi", Version = "v1" });
-    opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        In = ParameterLocation.Header,
-        Description = "Please enter token",
-        Name = "Authorization",
-        Type = SecuritySchemeType.Http,
-        BearerFormat = "JWT",
-        Scheme = "bearer"
-    });
-    opt.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        {
-            new OpenApiSecurityScheme
-            {
-                Reference = new OpenApiReference
-                {
-                    Type=ReferenceType.SecurityScheme,
-                    Id="Bearer"
-                }
-            },
-            new string[]{}
-        }
-    });
-});
 
 builder.Services.AddCors(options => options.AddPolicy(name: "FrontendOrigins",
     policy =>
