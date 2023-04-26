64f2a66fffba3c05c2b17a503e07ff6cfca6ceb8
trajcevr <trajcevr@yahoo.com>
2023-04-26 20:08:16 +0200

    Created Dtos for available models

--- LernDeutsch-Backend/Dtos/CourseCreateDto.cs
+++ LernDeutsch-Backend/Dtos/CourseCreateDto.cs
@@ -1,11 +1,16 @@
-﻿namespace LernDeutsch_Backend.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LernDeutsch_Backend.Dtos
 {
     public class CourseCreateDto
     {
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string Level { get; set; }
         public int Length { get; set; }
         public double Price { get; set; }
+        [Required]
         public Guid TutorId { get; set; }
     }
 }
---
+++ LernDeutsch-Backend/Dtos/QuizCreateDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LernDeutsch_Backend.Dtos
+{
+    public class QuizCreateDto
+    {
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public int LessonId { get; set; }
+        public List<QuestionCreateDto> Questions { get; set; }
+    }
+}
