d2b3feb6c860ffc6bbf1f513f271d1ebd0270875
Petar <zafirovski.petar@gmail.com>
2023-05-03 19:53:28 +0200

    added question controller

--- LernDeutsch-Backend/Services/IQuestionService.cs
+++ LernDeutsch-Backend/Services/IQuestionService.cs
@@ -1,8 +1,10 @@
-﻿using LernDeutsch_Backend.Models;
+﻿using LernDeutsch_Backend.Dtos;
+using LernDeutsch_Backend.Models;
 using LernDeutsch_Backend.Services.Core;
 
 namespace LernDeutsch_Backend.Services;
 
 public interface IQuestionService : IBaseService<Question>
 {
+    Question CreateQuestion(QuestionCreateDto dto);
 }
